a11b45a348b4331ebc19b25c9c929e685f39c910
ArmanMDl <kzarman401@gmail.com>
2025-02-27 05:18:11 +0000

    add new file

--- Arman.py
+++ Arman.py
@@ -1,49 +1,35 @@
-def print_board(board):
-    for row in board:
-        print(" | ".join(row))
-        print("-" * 5)
+import random
 
-def check_winner(board):
-    # Проверяем строки, столбцы и диагонали
-    for row in board:
-        if row[0] == row[1] == row[2] and row[0] != " ":
-            return True
+def hangman():
+    words = ["python", "java", "javascript", "ruby", "html", "css"]
+    word = random.choice(words)
+    guessed = "_" * len(word)
+    attempts = 6
+    guessed_letters = []
 
-    for col in range(3):
-        if board[0][col] == board[1][col] == board[2][col] and board[0][col] != " ":
-            return True
+    print("Добро пожаловать в игру 'Виселица'!")
 
-    if board[0][0] == board[1][1] == board[2][2] and board[0][0] != " ":
-        return True
+    while attempts > 0 and "_" in guessed:
+        print(f"Слово: {guessed}")
+        print(f"Попыток осталось: {attempts}")
+        print(f"Использованные буквы: {', '.join(guessed_letters)}")
+        
+        letter = input("Введите букву: ").lower()
+        
+        if letter in guessed_letters:
+            print("Вы уже угадывали эту букву.")
+            continue
 
-    if board[0][2] == board[1][1] == board[2][0] and board[0][2] != " ":
-        return True
-
-    return False
-
-def tic_tac_toe():
-    board = [[" " for _ in range(3)] for _ in range(3)]
-    current_player = "X"
-    moves_left = 9
-
-    while moves_left > 0:
-        print_board(board)
-        row = int(input(f"Игрок {current_player}, введите номер строки (0-2): "))
-        col = int(input(f"Игрок {current_player}, введите номер столбца (0-2): "))
-
-        if board[row][col] == " ":
-            board[row][col] = current_player
-            moves_left -= 1
-            if check_winner(board):
-                print_board(board)
-                print(f"Игрок {current_player} победил!")
-                break
-            current_player = "O" if current_player == "X" else "X"
+        if letter in word:
+            guessed_letters.append(letter)
+            guessed = "".join([letter if word[i] == letter else guessed[i] for i in range(len(word))])
         else:
-            print("Эта клетка уже занята. Попробуйте снова.")
+            guessed_letters.append(letter)
+            attempts -= 1
 
-    if moves_left == 0:
-        print("Ничья!")
-        print_board(board)
+    if "_" not in guessed:
+        print(f"Поздравляю! Вы угадали слово: {word}")
+    else:
+        print(f"Вы проиграли. Загаданное слово было: {word}")
 
-tic_tac_toe()
+hangman()
